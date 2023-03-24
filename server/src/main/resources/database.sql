6a10927895f94a5caa8a014c63fd185d5a543a1b
victor3099 <s301607@studenti.polito.it>
2023-03-24 15:23:49 +0100

    feat(database): created product and profile tables

--- server/src/main/resources/database.sql
+++ server/src/main/resources/database.sql
@@ -7,22 +7,15 @@ CREATE TABLE IF NOT EXISTS Product (
 );
 
 
-CREATE EXTENSION citext;
-CREATE DOMAIN email_type AS citext CHECK(
-            VALUE ~ '^\w+@[a-zA-Z_]+?\[a-zA-Z]{2,3}$'
-        );
-
 CREATE TABLE IF NOT EXISTS Profile (
 
-    email email_type PRIMARY KEY,
+    email varchar(255) PRIMARY KEY,
     name varchar(255),
     surname varchar(255),
     password varchar(255),
     salt varchar(255)
 
 );
-
-INSERT INTO Profile VALUES ('matteo@wa.polito.it', 'Matteo', 'Rosani')
 
 
 
