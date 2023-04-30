d518748a22a50863b9dfcb7df78ed2dc00f6aeac
Jaco <jacodecristo1999@gmail.com>
2023-04-30 17:42:38 +0200

    feat(chatMessage): chat messages can contain attachments and retrieved

--- server/src/main/kotlin/it/polito/wa2/g19/server/ticketing/chat/ChatMessageDTO.kt
+++ server/src/main/kotlin/it/polito/wa2/g19/server/ticketing/chat/ChatMessageDTO.kt
@@ -3,24 +3,27 @@ package it.polito.wa2.g19.server.ticketing.chat
 import it.polito.wa2.g19.server.ticketing.attachments.AttachmentProjection
 import it.polito.wa2.g19.server.ticketing.attachments.StubAttachmentDTO
 import it.polito.wa2.g19.server.ticketing.attachments.toStubDTO
+import jakarta.persistence.Id
 import java.io.File
 import java.time.LocalDateTime
 
 abstract class ChatMessageDTO(
+
     var authorEmail: String,
     var body: String,
 )
 
-class ChatMessageInDTO(authorEmail: String, body: String, val attachments: Set<File>?):
+class ChatMessageInDTO(authorEmail: String, body: String):
     ChatMessageDTO(authorEmail, body)
 
-class ChatMessageOutDTO( authorEmail: String, body: String, val stubAttachments: Set<StubAttachmentDTO>?,
+class ChatMessageOutDTO(val id: Int, authorEmail: String, body: String, val stubAttachments: Set<StubAttachmentDTO>?,
                          val timestamp: LocalDateTime):
         ChatMessageDTO(authorEmail, body)
 
 
 
 fun ChatMessage.toOutDTO(attachmentsProjection: List<AttachmentProjection>) = ChatMessageOutDTO(
+    getId()!!,
     author.email,
     body,
     attachmentsProjection.map { it.toStubDTO()!! }.toSet(),
