db9faac2b97b0e8663a244f0a2200d7dee9e191b
matteorosani <matteo.rosani@gmail.com>
2023-05-04 11:20:53 +0200

    feat: Bug fix client api

--- client/src/API/api.tsx
+++ client/src/API/api.tsx
@@ -1,6 +1,7 @@
+import Profile from "../classes/Profile"
 
 async function getAllProducts(): Promise<Response>{
-    
+
     const response = await fetch("/API/products")
     return response
 
@@ -9,11 +10,35 @@ async function getAllProducts(): Promise<Response>{
 async function getProductByEAN(ean: string): Promise<Response>{
     const response = await fetch("/API/products/" + ean )
     return response
-    
+
 }
 
+async function getProfileByEmail(email: string): Promise<Response> {
 
+    const response = await fetch("/API/profiles/" + email)
+    return response
 
+}
 
-const API = {getAllProducts, getProductByEAN}
+async function postProfile(profile: Profile): Promise<Response> {
+    const response = await fetch("/API/profiles", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        body: JSON.stringify({email: profile.email, name: profile.name, surname: profile.surname})
+    })
+    console.log(response)
+    return response
+}
+
+async function putProfile(profile: Profile): Promise<Response> {
+    const response = await fetch("/API/profiles/" + profile.email, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        body: JSON.stringify({email: profile.email, name: profile.name, surname: profile.surname})
+    })
+
+    return response
+}
+
+const API = {getAllProducts, getProductByEAN, getProfileByEmail, postProfile, putProfile}
 export default API
