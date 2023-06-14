a3a1bccbabb13c7258b1572daa36637baee0874d
Vittorio Arpino <100307068+victor3099@users.noreply.github.com>
2023-06-14 13:03:54 +0200

    fix: open ticket and security filter chain

--- client/src/components/warranty/WarrantyCard.tsx
+++ client/src/components/warranty/WarrantyCard.tsx
@@ -1,8 +1,8 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Badge, Button, Col, Container, Row} from "react-bootstrap";
 import {Typography} from "@mui/material";
 import React from "react";
 import {Warranty, Duration} from "../../classes/Warranty";
-import {parseISO} from 'date-fns';
+import {addDays, addHours, addMinutes, addMonths, addSeconds, addWeeks, addYears, parseISO} from 'date-fns';
 import HasRole from "../authentication/HasRole";
 import {useNavigate} from "react-router-dom";
 
@@ -13,7 +13,39 @@ function WarrantyCard({warranty}: {
     const duration = Duration.fromString(warranty.duration)
     const creationTime = parseISO(warranty.creationTimestamp)
     const activationTime = parseISO(warranty.activationTimestamp)
+
+    function isExpired() {
+        let expiredDate = activationTime
+        if(duration.days != 0) {
+            expiredDate = addDays(expiredDate, duration.hours)
+        }
+        if(duration.months != 0) {
+            expiredDate = addMonths(expiredDate, duration.hours)
+        }
+        if(duration.years != 0) {
+            expiredDate = addYears(expiredDate, duration.hours)
+        }
+        if(duration.weeks != 0) {
+            expiredDate = addWeeks(expiredDate, duration.hours)
+        }
+        if(duration.hours != 0) {
+            expiredDate = addHours(expiredDate, duration.hours)
+        }
+        if(duration.minutes != 0) {
+            expiredDate = addMinutes(expiredDate, duration.hours)
+        }
+        if(duration.seconds != 0) {
+            expiredDate = addSeconds(expiredDate, duration.hours)
+        }
+        return new Date(Date.now()) < expiredDate
+    }
+
     return <Container className={"border border-3 rounded border-primary"}>
+        <Row className={"pt-3 ms-1"} style={{display: "flex", justifyContent: "left"}}>
+            <Col>
+                {!isExpired() ? <h4><Badge bg={"danger"}>Expired</Badge></h4> : <h4><Badge bg={"success"}>Valid</Badge></h4>}
+            </Col>
+        </Row>
         <Row className={"ps-3 mt-3"}>
             <Typography variant="h5" component="div" color="primary">
                 <strong>ID</strong>
@@ -80,7 +112,7 @@ function WarrantyCard({warranty}: {
         </Row>
         <HasRole role={"Client"}>
             <Row className={"p-3"}>
-                <Col>
+                {isExpired() ? <Col>
                     <Button variant={"primary"} onClick={() => navigate("/tickets/add", {
                         state: {
                             warranty: warranty
@@ -88,7 +120,7 @@ function WarrantyCard({warranty}: {
                     })}>
                         Open ticket
                     </Button>
-                </Col>
+                </Col> : <></>}
             </Row>
         </HasRole>
     </Container>
